89af888ba145bf27b7dc20a7f7fd2acef0d915d5
Gameringly <luke.x.x.williams@gmail.com>
2025-04-29 23:17:35 +0100

    Act clear music
    Adds the new act clear music

--- Scripts/Global/Global.gd
+++ Scripts/Global/Global.gd
@@ -45,7 +45,7 @@ var effectTheme = null
 var drowning = null
 var life = null
 # song themes to play for things like invincibility and speed shoes
-var themes = [preload("res://Audio/Soundtrack/1. SWD_Invincible.ogg"),preload("res://Audio/Soundtrack/2. SWD_SpeedUp.ogg"),preload("res://Audio/Soundtrack/4. SWD_StageClear.ogg")]
+var themes = [preload("res://Audio/Soundtrack/1. SWD_Invincible.ogg"),preload("res://Audio/Soundtrack/2. SWD_SpeedUp.ogg"),preload("res://Audio/Soundtrack/Act Clear! (Sabre94).wav")]
 # index for current theme
 var currentTheme = 0
 
