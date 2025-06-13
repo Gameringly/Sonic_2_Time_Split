1e2cc2fb4452ecc8f3e9d001cefc9ed77e07bdeb
Gameringly <luke.x.x.williams@gmail.com>
2025-06-13 20:37:11 +0100

    dandelion sprites and tile update
    Adds sprites for the dandelion gimmick, sprites for sonic for the gimmick, new pillar tiles and fix the monitors being displaced

--- Scripts/Gimmicks/Dandelion.gd
+++ Scripts/Gimmicks/Dandelion.gd
@@ -1,6 +1,7 @@
 extends Area2D
 
 @onready var timer = $Timer
+@onready var cover = $Cover
 
 var flying = false
 var speed = 0
@@ -11,8 +12,15 @@ var player
 func _physics_process(delta: float) -> void:
 	if flying:
 		player.movement = speed
-		position = Vector2(player.position.x, player.position.y+30)
+		if player.direction >= 1:
+			position = Vector2(player.position.x+2, player.position.y+20)
+		else:
+			position = Vector2(player.position.x-16, player.position.y+20)
+		cover.modulate.a = timer.time_left / 1.5
+		if player.animator.current_animation != "dandelion":
+			player.animator.play("dandelion")
 		if player.horizontalSensor.is_colliding() or (player.ground and player.movement.y > 0) or player.movement != speed:
+			player.animator.play("roll")
 			flying = false
 			queue_free()
 		
@@ -29,12 +37,14 @@ func _on_body_entered(body: Node2D) -> void:
 		timer.start()
 		player.set_state(player.STATES.AIR)
 	elif flying:
+		player.animator.play("roll")
 		flying = false
 		queue_free()
 	
 
 
 func _on_timer_timeout() -> void:
+	player.animator.play("roll")
 	flying = false
 	queue_free()
 	
