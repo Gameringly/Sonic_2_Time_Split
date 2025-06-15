785f74357716ea2411e542e874142212e442f2c4
Gameringly <luke.x.x.williams@gmail.com>
2025-06-15 21:58:21 +0100

    Dandelion seed particles
    Add particles to dandelion gimmick

--- Scripts/Gimmicks/Dandelion.gd
+++ Scripts/Gimmicks/Dandelion.gd
@@ -2,6 +2,8 @@ extends Area2D
 
 @onready var timer = $Timer
 @onready var cover = $Cover
+@onready var particles = $Particles
+@onready var seedFrames = $SeedFrames
 
 var flying = false
 var speed = 0
@@ -16,7 +18,11 @@ func _physics_process(delta: float) -> void:
 			position = Vector2(player.position.x+2, player.position.y+20)
 		else:
 			position = Vector2(player.position.x-16, player.position.y+20)
+		#make the cover fade
 		cover.modulate.a = timer.time_left / 1.5
+		#change the frame of the particles to the current frame in the animated sprite
+		particles.texture = seedFrames.get_sprite_frames().get_frame_texture("default", seedFrames.get_frame())
+		
 		if player.animator.current_animation != "dandelion":
 			player.animator.play("dandelion")
 		if player.horizontalSensor.is_colliding() or (player.ground and player.movement.y > 0) or player.movement != speed:
@@ -34,6 +40,7 @@ func _on_body_entered(body: Node2D) -> void:
 		player = body
 		speed = player.movement.rotated(player.angle-player.gravityAngle)
 		flying = true
+		particles.emitting = true
 		timer.start()
 		player.set_state(player.STATES.AIR)
 	elif flying:
